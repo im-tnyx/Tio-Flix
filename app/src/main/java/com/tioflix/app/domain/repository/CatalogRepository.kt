71de9467eed5469b68eeb62f5ac3d363c4df0415
Santosh Jangid <tnyxapp@gmail.com>
2026-07-12 01:10:25 +0530

    Add series seasons repository contract

--- app/src/main/java/com/tioflix/app/domain/repository/CatalogRepository.kt
+++ app/src/main/java/com/tioflix/app/domain/repository/CatalogRepository.kt
@@ -1,7 +1,9 @@
 package com.tioflix.app.domain.repository
 
 import com.tioflix.app.domain.model.HomeCatalog
+import com.tioflix.app.domain.model.SeriesSeason
 
 interface CatalogRepository {
     suspend fun getHomeCatalog(): Result<HomeCatalog>
+    suspend fun getSeriesSeasons(contentId: String): Result<List<SeriesSeason>>
 }
