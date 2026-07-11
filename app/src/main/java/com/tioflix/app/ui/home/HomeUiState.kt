3704eece9f714306e4320d2e3fc0324566ceaa81
Santosh Jangid <tnyxapp@gmail.com>
2026-07-11 17:45:57 +0530

    Add home UI state

---
+++ app/src/main/java/com/tioflix/app/ui/home/HomeUiState.kt
@@ -0,0 +1,7 @@
+package com.tioflix.app.ui.home
+
+data class HomeUiState(
+    val title: String = "Tio-Flix",
+    val subtitle: String = "Android Mobile, TV and Fire TV foundation is ready.",
+    val isLoading: Boolean = false
+)
