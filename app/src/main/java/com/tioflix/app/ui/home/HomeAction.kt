50f684f5f3243fc13da718802222174d5d5e9577
Santosh Jangid <tnyxapp@gmail.com>
2026-07-16 13:37:10 +0530

    Add home search action

--- app/src/main/java/com/tioflix/app/ui/home/HomeAction.kt
+++ app/src/main/java/com/tioflix/app/ui/home/HomeAction.kt
@@ -3,5 +3,6 @@ package com.tioflix.app.ui.home
 sealed interface HomeAction {
     data object RetryClicked : HomeAction
     data object LogoutClicked : HomeAction
+    data object SearchClicked : HomeAction
     data class ContentClicked(val contentId: String) : HomeAction
 }
